63429d24ca29461096b324c652a66f64516d5d00
NitroxMk <60615443+NitroxMk@users.noreply.github.com>
2021-05-26 09:27:18 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  json =[
+    { "titre":"Ford", "content": "desc", "loveit":"-1","createat":"11/05/2020" },
+    { "titre":"Ford3", "content": "desc", "loveit":"2","createat":"10/05/2020" },
+    { "titre":"Ford4", "content": "desc", "loveit":"-2","createat":"16/05/2020" },
+  { "titre":"Ford4", "content": "desc", "loveit":"1","createat":"1/05/2020" }
+  ];
   title = 'app';
 }
